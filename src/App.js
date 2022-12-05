97da4912b508434c1a259787671520323c7b0661
Thomas Muller <bucky30@hotmail.fr>
2022-12-05 16:46:33 +0100

    Ajout de la route Error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Error from "./components/Error"
 
 function App() {
   return (
     <>
+    <Router>
       <Header />
-
+      <Routes>
+        {/* <Route path='' element={<Home />} />
+        <Route path='' element={} />
+        <Route path='' element={} /> */}
+        <Route path='*' element={<Error />} />
+      </Routes>
       <Footer />
+      </Router>
     </>
   );
 }
