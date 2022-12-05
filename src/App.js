87518a05326d388eb35df77a57bf6428af1fcda2
Thomas Muller <bucky30@hotmail.fr>
2022-12-05 16:29:45 +0100

    Ajout de react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { useEffect, useState } from "react"
 
 function App() {
   return (
     <>
       <Header />
-      
+
       <Footer />
     </>
   );
