93dd82fb7d16712ebb67544b68f9d5743679241a
Thomas Muller <bucky30@hotmail.fr>
2022-12-20 00:57:27 +0100

    Ajout de la fonction souligner du menu nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import logo from "../assets/logo.svg"
 import '../styles/Header.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+
     return (
         <header>
             <img id="logoHeader" src={logo} alt="Kasa" />
             <nav>
-                <Link to='' className="navPrincipale" id="navAccueil">Accueil</Link>
-                <Link to='/about' className="navPrincipale" id="navAbout">A propos</Link>
+                <NavLink to='' activeClassName="active" id="navAccueil" >Accueil</NavLink>
+                <NavLink to='/about' activeClassName="active" id="navAbout" >A propos</NavLink>
             </nav>
         </header>
     )
